11d0d57e1dd8090287011e8ad499635ce74a7601
anselmi-dev <anselmi.dev@gmail.com>
2022-07-06 11:00:19 -0300

    Agregó el subtitulo en el bloque 'title' del Nova

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,11 +1,19 @@
-<div class="relative my-4 lg:py-10">
+<div class="relative my-4 lg:my-0 lg:py-5">
     <div class="mx-auto max-w-7xl px-4 sm:px-6">
         <div class="text-center">
-            <h1 class="font-bold text-white">
-                <span class="block text-2xl lg:text-4xl leading-tight">
-                    {{ $content->title }}
-                </span>
-            </h1>
+            @if(isset($content->title) && !is_null($content->title))
+                <h1 class="font-bold text-white">
+                    <span class="block text-2xl lg:text-4xl leading-tight">
+                        {{ $content->title }}
+                    </span>
+                </h1>
+            @endif
+
+            @if(isset($content->subtitle) && !is_null($content->subtitle))
+                <div class="block w-full text-lg text-gray-100">
+                    <span>{{ $content->subtitle }}</span>
+                </div>
+            @endif
             <hr class="my-5 block">
         </div>
     </div>
